cacf749a21e4d2075bf090a2085f9fe6fb955f1c
noelnp <noel.peltomaki@gmail.com>
2025-09-04 12:06:20 +0200

    Added validation to CreateBetRequest

--- src/main/java/com/noelnp/sportsbooklite/dto/bet/CreateBetRequest.java
+++ src/main/java/com/noelnp/sportsbooklite/dto/bet/CreateBetRequest.java
@@ -1,6 +1,16 @@
 package com.noelnp.sportsbooklite.dto.bet;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
-public record CreateBetRequest(Long userId, long eventId, BigDecimal odds, BigDecimal amount) {
-}
+public record CreateBetRequest(
+        @NotNull Long userId,
+
+        @NotNull Long eventId,
+
+        @NotNull @Positive BigDecimal odds,
+
+        @NotNull @Positive BigDecimal amount
+) {}
