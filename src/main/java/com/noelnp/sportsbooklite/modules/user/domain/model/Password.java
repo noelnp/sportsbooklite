c5ee213dc1c7bda1c84ec7ab3192a54bc9535ffa
noelnp <noel.peltomaki@gmail.com>
2025-09-06 16:36:03 +0200

    Create value objects for user: Email, Password, PasswordHash. Created new exception: InvalidEmailException

---
+++ src/main/java/com/noelnp/sportsbooklite/modules/user/domain/model/Password.java
@@ -0,0 +1,4 @@
+package com.noelnp.sportsbooklite.modules.user.domain.model;
+
+public record Password() {
+}
---
+++ src/main/java/com/noelnp/sportsbooklite/modules/user/domain/model/PasswordHash.java
@@ -0,0 +1,4 @@
+package com.noelnp.sportsbooklite.modules.user.domain.model;
+
+public class PasswordHash {
+}
